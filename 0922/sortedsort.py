f1d9914ada098f28fe7aa7665d1a696dada76f08
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-22 23:49:02 +0900

    이진탐색 끝

---
+++ 0922/sortedsort.py
@@ -0,0 +1,23 @@
+#sorted 소스코드
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+result = sorted(array)
+print(result)
+
+# sort 소스코드
+
+array1 = [7,5,9,9,3,1,6,2,4,8]
+
+array1.sort()
+print(array1)
+
+
+# 정렬 라이브러리에서 key를 활용한 소스코드
+array3 = [('바나나',2),('사과',5),('당근',3)]
+
+def setting(data):
+    return data[0]
+
+result = sorted(array3, key=setting)
+print(result)
---
+++ 0922/반복문이진탐색.py
@@ -0,0 +1,17 @@
+# 이진 탐색 소스코드 구현(반복문)
+
+def binary_search(array,target,start,end):
+    while start <= end:
+        mid = (start+end)//2
+
+        if array[mid] == target:
+            return mid
+        
+        elif array[mid]>target:
+            end=mid-1
+        else:
+            start=mid+1
+    return None
+
+
+    
