2628226a29f0ede155dc10a0de991a3210d33f62
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-22 17:23:45 +0900

    enumerate , 인강 3묹[

--- 0920/퀵정렬.py
+++ 0920/퀵정렬.py
@@ -23,5 +23,28 @@ def quick_sort(array, start, end):
             array[left], array[right] = array[right], array[left]
         
 #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
-quick_sort(array, start, right-1)
-quick_sort(array, right+1,end)
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1,end)
+
+quick_sort(array,0,len(array)-1)
+print(array)
+
+#####################################################################
+#파이썬의 장점을 살려 짧게 작성한 퀵 정렬 소스 코드
+
+array = [5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array):
+    #리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side)+ [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
---
+++ 0921/enumerate.py
@@ -0,0 +1,6 @@
+arr = [65,73,86,34,44]
+
+for idx , x in enumerate(arr):
+  print('인덱스의 값',idx, '값',x)
+
+  
