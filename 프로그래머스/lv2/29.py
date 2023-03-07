b00fc6a61e6315664aefdcd50ed5c2a4309eacdf
kanngji <alleneffort1994@gmail.com>
2023-03-07 14:45:47 +0900

    알고리즘 3문제

---
+++ 프로그래머스/lv2/29.py
@@ -0,0 +1,10 @@
+# n ^ 2 배열 자르기
+
+def solution(n, left, right):
+    answer = []
+    for i in range(left,right+1):
+        s,r = divmod(i,n)
+        if s>=r: answer.append(s+1)
+        else:
+            answer.append(r+1)
+    return answer
