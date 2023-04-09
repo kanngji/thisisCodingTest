5702f5a59b0ac07c224438d01f8fb0161d37454a
kanngji <alleneffort1994@gmail.com>
2023-04-10 00:51:46 +0900

    알고리즘 3문제

---
+++ 프로그래머스/lv3/4.py
@@ -0,0 +1,37 @@
+# 네트워크
+def solution(n, computers):
+    
+    def DFS(i):
+        visited[i]=1
+        for a in range(n):
+            if computers[i][a] and not visited[a]:
+                DFS(a)
+    answer = 0
+    visited=[0 for i in range(len(computers))]
+    for i in range(n):
+        if not visited[i]:
+            DFS(i)
+            answer+=1
+            
+    return answer
+
+from collections import deque
+
+def solution(n,computers):
+
+    def BFS(i):
+        q=deque()
+        q.append(i)
+        while q:
+            i=q.popleft()
+            visited[i]=1
+            for a in range(n):
+                if computers[i][a] and not visited[a]:
+                    q.append(a)
+    answer = 0
+    visited = [0 for i in range(len(computers))]
+    for i in range(n):
+        if not visited[i]:
+            BFS(i)
+            answer+=1
+    return answer
